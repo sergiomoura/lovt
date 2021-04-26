0046044506e8f14fee42bdd86b84ef7b3020bff9
Hendy Almeida <hendy.almeida@hotmail.com>
2021-04-26 10:26:23 -0300

    js front

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//console.log('JS externo funcionou!');
+//console.log(window);
+
+//alert('Seja bem vinde!'); // mensagem para usuário
+
+//let adulto = confirm('Você tem mais de 18 anos?'); // retorna booleano
+
+let nomeUsuario = prompt('Qual seu nome?'); // retorna texto inserido pelo usuario
+console.log(nomeUsuario);
+
+let elementHeader = document.querySelector('.olaUsuario'); // seleciona o elemento html
+console.log(elementHeader);
+
+elementHeader.innerText = `Olá, ${nomeUsuario}`; // atribui conteudo ao elemento selecionado
+
+
+
